fc056edadb2b68d9f1fd84a1b926c7a79b1d5d55
Gabriele Pagliaricci <118124495+mindthediv@users.noreply.github.com>
2023-09-14 10:43:33 +0200

    align cv btn

--- pf-app/app/(components)/Header.tsx
+++ pf-app/app/(components)/Header.tsx
@@ -10,7 +10,11 @@ export default function Header() {
           <h1>Gabriele Pagliaricci</h1>
           <h4 className="py-2">Full Stack Web Developer</h4>
         </Col>
-        <Col xs={12} md={4} className="d-flex align-items-center">
+        <Col
+          xs={12}
+          md={4}
+          className="d-flex align-items-center justify-content-center"
+        >
           <a href="/assets/curriculum.pdf" target="_blank">
             <h3 className="my-btn p-3 m-auto">See CV</h3>
           </a>
