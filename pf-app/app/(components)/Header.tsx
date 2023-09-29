4b11cba443d61435b5067defa3c8cfc152af8598
Gabriele Pagliaricci <118124495+mindthediv@users.noreply.github.com>
2023-09-29 12:37:46 +0200

    Cv update

--- pf-app/app/(components)/Header.tsx
+++ pf-app/app/(components)/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
           md={4}
           className="d-flex align-items-center justify-content-center"
         >
-          <a href="/assets/curriculum.pdf" target="_blank">
+          <a href="/assets/Gabriele Pagliaricci CV.pdf" target="_blank">
             <h3 className="my-btn p-3 m-auto">See CV</h3>
           </a>
         </Col>
